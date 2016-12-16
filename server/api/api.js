85cbb366639d0773811fc7d7525d815a6f2c849d
Ubuntu <ubuntu@ip-172-31-8-83.us-west-1.compute.internal>
2016-12-16 21:38:57 +0000

    global error handler

--- server/api/api.js
+++ server/api/api.js
@@ -3,10 +3,18 @@ var router = require('express').Router();
 // api router will mount other routers
 // for all our resources
 
-//This is the initial API Endpoint 
+//This is the initial API Endpoint
 router.get("/",function(req,res){
     res.json({"error" : false,"message" : "Hello World"});
 });
 router.use('/users', require('./user/userRoutes'));
- 
+router.use('/posts', require('./post/postRoutes'));
+router.use('/categories', require('./category/categoryRoutes'));
+
 module.exports = router;
+module.errorHandler = errorHandler;
+
+function errorHandler(err, res){
+  res.status(500);
+  res.send("Internal error");
+}
--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	// disbable logging for testing
 logging: false,
   db: {
-        // the database url to connect!                                                                                                                                              
-        url: 'mongodb://ec2-52-44-183-206.compute-1.amazonaws.com:27017/finals-test'
+        // the database url to connect!
+        url: 'mongodb://ec2-54-153-77-110.us-west-1.compute.amazonaws.com:27017/finals'
     }
 }
